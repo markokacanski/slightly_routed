aa892565c3714ea044304b35cbd04f16eab0e2f8
Marko Kacanski <marko.kacanski@gmail.com>
2013-01-18 15:49:10 +0100

    imported bootstrap, jquery, jquery UI from CDNs so it's always up to date and served over the net, and initialized assets folder with default.css

--- site/layout.php
+++ site/layout.php
@@ -5,9 +5,18 @@
 <title>TITLE GOES HERE</title>
 <meta name="keywords" content="" />
 <meta name="description" content="" />
+ <script src="//ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js"></script>
+  <script src="//ajax.googleapis.com/ajax/libs/jqueryui/1.9.2/jquery-ui.min.js"></script>
+  <link href="//netdna.bootstrapcdn.com/twitter-bootstrap/2.2.2/css/bootstrap-combined.min.css" rel="stylesheet">
+  <script src="//netdna.bootstrapcdn.com/twitter-bootstrap/2.2.2/js/bootstrap.min.js"></script>
 <link href="default.css" rel="stylesheet" type="text/css" />
 </head>
 <body>
+<p>slightly_routed here!</p>
+<p>If the template system is working, you should see the page content below:</p>
+<hr />
 <?php echo $yield; ?>
+<hr />
+<p>that the page you requested. I hope you're enjoying the framework! </p>
 </body>
 </html>
